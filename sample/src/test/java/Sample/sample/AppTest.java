9b3459d9d1320d185b0f8a520eb10e88943bd73b
etg-soujanya <etg-soujanya@ETG-Soujanya>
2021-03-26 20:30:07 +0530

    added all files

---
+++ sample/src/test/java/Sample/sample/AppTest.java
@@ -0,0 +1,15 @@
+package Sample.sample;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+	public void testApp()
+    {
+    	System.out.println("hello world");
+    }
+}

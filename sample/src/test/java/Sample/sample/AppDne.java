aa09327089240ccb18e056665b4d91cadbdec8f8
SoujanyaPuligilla96 <chitti6.puligilla@gmail.com>
2021-03-26 22:53:45 +0530

    added headlesss browser

--- sample/src/test/java/Sample/sample/AppDne.java
+++ sample/src/test/java/Sample/sample/AppDne.java
@@ -1,5 +1,6 @@
 package Sample.sample;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,7 +18,10 @@ public class AppDne {
 	 @Test
 	 public void chromeTest() {
 		 WebDriverManager.chromedriver().setup();
-		 ChromeDriver driver = new ChromeDriver();
+		 ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless");
+
+		 ChromeDriver driver = new ChromeDriver(options);
 			driver.get("https://www.sugarcrm.com/request-demo/");
 			System.out.print(driver.getTitle());
 			driver.close();
